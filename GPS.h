9b8af89d0e2369d8e6e7312fc43127bfa8f489e6
Michael Black <brushyfork23@gmail.com>
2022-12-13 13:42:32 -0800

    move GPS handling into a library

---
+++ GPS.h
@@ -0,0 +1,60 @@
+#ifndef GPS_h
+#define GPS_h
+
+#include <Adafruit_GPS.h>
+#define GPSSerial Serial1 // RX: A0, TX: A1
+
+class GPS {
+
+private:
+    Adafruit_GPS gps = Adafruit_GPS(&GPSSerial);
+
+public:
+    void setup() {
+        Serial.println(F("Initializing GPS"));
+        gps.begin(9600);
+        // request RMC (recommended minimum) and GGA (fix data) including altitude.
+        // Can we get away with just PMTK_SET_NMEA_OUTPUT_RMCONLY (minimum recommended only)?
+        gps.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+        gps.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+        Serial.println(F("GPS Initialized!"));
+    }
+
+    void loop() {
+        // Read new character from GPS module
+        gps.read();
+    }
+
+    bool hasFix() {
+        return gps.fix;
+    }
+
+    String lat() {
+        return String(
+            String(gps.latitude, 4) +
+            String(gps.lat)
+        );
+    }
+
+    String lon() {
+        return String(
+            String(gps.longitude, 4) +
+            String(gps.lon)
+        );
+    }
+
+    String time() {
+        return String(
+            String((gps.hour < 10 ? "0" : "")) +
+            gps.hour +
+            String(":") +
+            String((gps.minute < 10 ? "0" : "")) +
+            gps.minute +
+            String(":") +
+            String((gps.seconds < 10 ? "0" : "")) +
+            gps.seconds
+        );
+    }
+};
+
+#endif
--- Sunstone.ino
+++ Sunstone.ino
@@ -14,11 +14,11 @@
 Display display;
 
 // GPS
-#include <Adafruit_GPS.h>
-#define GPSSerial Serial1 // RX: A0, TX: A1
-Adafruit_GPS GPS(&GPSSerial);
-// GPS Display Update Timer
-#define GPS_TIMER_MILLIS 500000 // 5 seconds
+#include "GPS.h"
+GPS gps;
+
+// GPS display refresh timer
+#define GPS_TIMER_MICROS 5000000 // 5 seconds
 hw_timer_t* gpsTimer = NULL;
 volatile SemaphoreHandle_t gpsTimerSemaphore;
 void ARDUINO_ISR_ATTR onGpsTimer(){
@@ -39,28 +39,21 @@ void setup() {
     display.print("Sunstone");
 
     // Initialize GPS
-    Serial.println(F("Initializing GPS"));
-    GPS.begin(9600);
-    // request RMC (recommended minimum) and GGA (fix data) including altitude.
-    // Can we get away with just PMTK_SET_NMEA_OUTPUT_RMCONLY (minimum recommended only)?
-    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
-    // Initialize GPS Timer
+    gps.setup();
+
+    // Initialize GPS display refresh timer
     gpsTimerSemaphore = xSemaphoreCreateBinary();
-    gpsTimer = timerBegin(0, 800, true); // prescaler of 800 configures alarm time to milliseconds
+    gpsTimer = timerBegin(1, 80, true); // prescaler of 80 configures alarm time to microseconds
     timerAttachInterrupt(gpsTimer, &onGpsTimer, true);
-    timerAlarmWrite(gpsTimer, GPS_TIMER_MILLIS, true);
+    timerAlarmWrite(gpsTimer, GPS_TIMER_MICROS, true);
     timerAlarmEnable(gpsTimer);
-    Serial.println(F("GPS Initialized!"));
 
     Serial.println(F("Setup Complete!"));
     delay(200);
 }
 
 void loop() {
-    // Read GPS
-    GPS.read();
-
+    gps.loop();
     display.loop();
 
     // Update displayed GPS location
@@ -71,29 +64,13 @@ void loop() {
       if (xSemaphoreTake(gpsTimerSemaphore, 0) == pdTRUE) {
         // fetch the latest GPS data
         Serial.print(F("Time: "));
-        if (GPS.hour < 10) { Serial.print('0'); }
-        Serial.print(GPS.hour, DEC); Serial.print(':');
-        if (GPS.minute < 10) { Serial.print('0'); }
-        Serial.print(GPS.minute, DEC); Serial.print(':');
-        if (GPS.seconds < 10) { Serial.print('0'); }
-        Serial.print(GPS.seconds, DEC); Serial.print(F(" "));
-        Serial.print("Date: ");
-        Serial.print(GPS.day, DEC); Serial.print('/');
-        Serial.print(GPS.month, DEC); Serial.print("/20");
-        Serial.println(GPS.year, DEC);
-        Serial.print("Fix: "); Serial.print((int)GPS.fix);
-        Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-        if (GPS.fix) {
-          Serial.print("Location: ");
-          Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-          Serial.print(", ");
-          Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-          Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-          Serial.print("Angle: "); Serial.println(GPS.angle);
-          Serial.print("Altitude: "); Serial.println(GPS.altitude);
-          Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-          Serial.print("Antenna status: "); Serial.println((int)GPS.antenna);
-        }
+        Serial.println(gps.time());
+        Serial.print("Fix: ");
+        Serial.println((int)gps.hasFix());
+        Serial.print("Lat: ");
+        Serial.println(gps.lat());
+        Serial.print("Lon: ");
+        Serial.println(gps.lon());
         Serial.println();
       }
     }
